06d8977b2252f6390daa1db8c293f24974476f52
Oliver Speir <oliverspeir9@gmail.com>
2023-02-09 19:48:58 -0700

    model

--- cookie_stand_api/models.py
+++ cookie_stand_api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+import random
 
 
 class CookieStand(models.Model):
@@ -18,3 +19,18 @@ class CookieStand(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', args=[str(self.id)])
+
+    def save(self, *args, **kwargs):
+
+        if not self.pk and not self.hourly_sales:
+            min = self.minimum_customers_per_hour
+            max = self.maximum_customers_per_hour
+
+            cookies_each_hour = [
+                int(random.randint(min, max) * self.average_cookies_per_sale)
+                for _ in range(14)
+            ]
+
+            self.hourly_sales = cookies_each_hour
+
+        super().save(*args, **kwargs)
